6fab9b84d10277a20d0ba97a8584a4cbb03461ae
Harry Payne <harryfpayne@gmail.com>
2021-12-08 11:05:06 +0000

    coordinates

--- common/common.graphql.go
+++ common/common.graphql.go
@@ -80,3 +80,68 @@ func (msg *Money) XXX_GraphqlArgs() gql.FieldConfigArgument {
 func (msg *Money) XXX_Package() string {
 	return "common"
 }
+
+var CoordinateGraphqlType = gql.NewObject(gql.ObjectConfig{
+	Name: "Coordinate",
+	Fields: gql.Fields{
+		"lat": &gql.Field{
+			Type: gql.NewNonNull(gql.Float),
+		},
+		"lng": &gql.Field{
+			Type: gql.NewNonNull(gql.Float),
+		},
+	},
+})
+
+var CoordinateGraphqlInputType = gql.NewInputObject(gql.InputObjectConfig{
+	Name: "CoordinateInput",
+	Fields: gql.InputObjectConfigFieldMap{
+		"lat": &gql.InputObjectFieldConfig{
+			Type: gql.NewNonNull(gql.Float),
+		},
+		"lng": &gql.InputObjectFieldConfig{
+			Type: gql.NewNonNull(gql.Float),
+		},
+	},
+})
+
+var CoordinateGraphqlArgs = gql.FieldConfigArgument{
+	"lat": &gql.ArgumentConfig{
+		Type: gql.NewNonNull(gql.Float),
+	},
+	"lng": &gql.ArgumentConfig{
+		Type: gql.NewNonNull(gql.Float),
+	},
+}
+
+func CoordinateFromArgs(args map[string]interface{}) *Coordinate {
+	return CoordinateInstanceFromArgs(&Coordinate{}, args)
+}
+
+func CoordinateInstanceFromArgs(objectFromArgs *Coordinate, args map[string]interface{}) *Coordinate {
+	if args["lat"] != nil {
+		val := args["lat"]
+		objectFromArgs.Lat = float64(val.(float64))
+	}
+	if args["lng"] != nil {
+		val := args["lng"]
+		objectFromArgs.Lng = float64(val.(float64))
+	}
+	return objectFromArgs
+}
+
+func (objectFromArgs *Coordinate) FromArgs(args map[string]interface{}) {
+	CoordinateInstanceFromArgs(objectFromArgs, args)
+}
+
+func (msg *Coordinate) XXX_GraphqlType() *gql.Object {
+	return CoordinateGraphqlType
+}
+
+func (msg *Coordinate) XXX_GraphqlArgs() gql.FieldConfigArgument {
+	return CoordinateGraphqlArgs
+}
+
+func (msg *Coordinate) XXX_Package() string {
+	return "common"
+}
