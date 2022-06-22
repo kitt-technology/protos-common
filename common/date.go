590ef0fb8226d7a59dfca8f9961761f053b550a1
Manas Sarpatwar <manas.sarpatwar@kittoffices.com>
2022-06-22 11:06:48 +0100

    add helpers

--- common/date.go
+++ common/date.go
@@ -5,3 +5,22 @@ import "time"
 func (date *Date) ToTime() time.Time {
 	return time.Date(int(date.Year), time.Month(date.Month), int(date.Day), 0, 0, 0, 0, time.UTC)
 }
+
+func ToCommonDate(t time.Time) *Date {
+	return &Date{
+		Year:  int32(t.Year()),
+		Month: int32(t.Month()),
+		Day:   int32(t.Day()),
+	}
+}
+
+func ToNullableCommonDate(t *time.Time) *Date {
+	if t == nil {
+		return nil
+	}
+	return &Date{
+		Year:  int32(t.Year()),
+		Month: int32(t.Month()),
+		Day:   int32(t.Day()),
+	}
+}
